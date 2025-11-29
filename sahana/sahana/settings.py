4b0d8f058b9d91e758dd4d2ee95d34a46ee1a29b
sahana <sahanasathyanarayan2007@gmail.com>
2025-11-29 09:05:41 +0530

    Success

--- sahana/sahana/settings.py
+++ sahana/sahana/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'startapp/static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'slotapp/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
